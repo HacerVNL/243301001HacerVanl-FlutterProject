dc02cb176e4ff78456f18dfd16b09bf6fdb8e88f
HacerVNL <hacervanli7@gmail.com>
2026-05-22 19:37:49 +0300

    kampanya ekleme ekranı tamamlandı

--- lib/ekle.dart
+++ lib/ekle.dart
@@ -69,7 +69,7 @@ class _EklemeEkraniState extends State<EklemeEkrani> {
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.pink[700],
+                  backgroundColor: Colors.pink[800],
                 ),
                 child: const Text(
                   "KAMPANYAYI EKLE",
